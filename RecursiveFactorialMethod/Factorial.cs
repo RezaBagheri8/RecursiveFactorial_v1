5c3c84455576e0fcd8b6baf05b562668062ad44e
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-23 11:53:37 +0430

    Initial Repo

---
+++ RecursiveFactorialMethod/Factorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveFactorialMethod
+{
+    public class Factorial
+    {
+
+        public int Calculate(int number)
+        {
+            if (number == 0)
+                return 1;
+            else
+                return number * Calculate(number - 1);
+
+        }
+    }
+}
---
+++ RecursiveFactorialMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using RecursiveFactorialMethod;
+
+Console.WriteLine("Please enter the number : ");
+
+var userInput = Console.ReadLine();
+
+var factorial = new Factorial();
+
+var result = factorial.Calculate(int.Parse(userInput));
+
+Console.WriteLine(result);
